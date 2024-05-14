6053210e75c63e6eb88cc9aa6681e7d61850fbe5
Caltagirone Charles <charles.dewidehem-caltagirone@laplateforme.io>
2024-05-14 14:30:59 +0200

    recycle view

--- app/src/main/java/com/example/newappcdamut/NoteListActivity.kt
+++ app/src/main/java/com/example/newappcdamut/NoteListActivity.kt
@@ -1,12 +1,18 @@
 package com.example.newappcdamut
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class NoteListActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var notes: MutableList<Note>
+    lateinit var adapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,22 @@ class NoteListActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        notes = mutableListOf<Note>()
+        notes.add(Note("Note 1", "Ma première note"))
+        notes.add(Note("Note 2", "Ma deuxième note"))
+        notes.add(Note("Note 3", "Ma troisième note"))
+
+        adapter = NoteAdapter(notes, this)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.notes_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onClick(view: View) {
+        if(view.tag != null){
+            Log.i("No activity", "Pas d'activité")
         }
     }
 }
