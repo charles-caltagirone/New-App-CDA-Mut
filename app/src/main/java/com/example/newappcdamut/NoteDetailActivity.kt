fd297d18f9a3f3a70ec2d3daf6a49429b8e79001
Caltagirone Charles <charles.dewidehem-caltagirone@laplateforme.io>
2024-05-15 09:46:51 +0200

    create menu

--- app/src/main/java/com/example/newappcdamut/NoteDetailActivity.kt
+++ app/src/main/java/com/example/newappcdamut/NoteDetailActivity.kt
@@ -2,6 +2,8 @@ package com.example.newappcdamut
 
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -41,5 +43,19 @@ class NoteDetailActivity : AppCompatActivity() {
 
         titleView.text = note.title
         textView.text = note.text
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_note_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_save -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
